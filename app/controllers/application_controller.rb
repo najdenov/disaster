d4f9c4e41c5fe013519673de8c89899373386ae0
Anton Evangelatov <anton.evangelatov@gmail.com>
2013-10-23 20:32:20 +0200

    improvements for locale.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,27 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = session[:locale] || I18n.default_locale
     if params[:locale]
-      I18n.locale = params[:locale] || I18n.default_locale
+      I18n.locale = params[:locale]
+    else
+      locale = extract_locale_from_accept_language_header
+      if locale == "en"
+        I18n.locale = locale.to_sym
+      end
+
+      if locale == "bg"
+        I18n.locale = locale.to_sym
+      end
     end
-    session[:locale] = I18n.locale
+
+    I18n.locale = :en unless [ :bg, :en ].include?(I18n.locale)
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first if request.env['HTTP_ACCEPT_LANGUAGE']
   end
 end
