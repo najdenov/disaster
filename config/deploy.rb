2462be3722eaa042791368d688fc510885a72b13
Anton Evangelatov <anton.evangelatov@gmail.com>
2013-10-23 22:26:42 +0200

    comment out assets precompilation.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,15 +29,15 @@ namespace :deploy do
     run "ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
   end
 
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
+  #namespace :assets do
+    #task :precompile, :roles => :web, :except => { :no_release => true } do
+      #from = source.next_revision(current_revision)
+      #if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        #run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      #else
+        #logger.info "Skipping asset pre-compilation because there were no asset changes"
+      #end
+    #end
   end
 
    task :restart, :roles => :app, :except => { :no_release => true } do
@@ -45,7 +45,7 @@ namespace :deploy do
    end
 end
 
-before "deploy:assets:precompile", "deploy:link_db"
+#before "deploy:assets:precompile", "deploy:link_db"
 
 after "deploy:restart", "deploy:cleanup"
 
