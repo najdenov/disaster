35e4b7fed184bc6aac504d084925542bd8071156
Anton Evangelatov <anton.evangelatov@gmail.com>
2013-12-23 12:06:18 +0100

    no limit for past events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def index
     @upcoming_events = Event.find(:all, :conditions => ["date>=?", Date.today], :order => "date desc", :limit => 10)
-    @past_events = Event.find(:all, :conditions => ["date<?", Date.today], :order => "date desc", :limit => 10)
+    @past_events = Event.find(:all, :conditions => ["date<?", Date.today], :order => "date desc")
   end
 
   def show
